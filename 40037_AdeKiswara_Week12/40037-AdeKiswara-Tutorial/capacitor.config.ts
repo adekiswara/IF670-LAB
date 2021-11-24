401537db7489c29257d030b4dcd288d19a2bba22
Ade Kiswara <ade.kiswara@student.umn.ac.id>
2021-11-24 18:51:34 +0700

    Mobile Cross Platform Week12

---
+++ 40037_AdeKiswara_Week12/40037-AdeKiswara-Tutorial/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '40037-AdeKiswara-Tutorial',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
